cbb8c4fa2f771069b29caef55e6d130b0a236e5a
Illia16 <illia2143@gmail.com>
2022-05-22 10:32:38 -0400

    init project setup

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="main">
+        Language app!
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+
+<style lang="scss">
+
+.main {
+    @apply bg-yellow-500;
+}
+</style>
