99ddeff56212ed7cf87f6667674a6aa85295a58e
Illia16 <illia2143@gmail.com>
2023-10-02 18:15:07 -0400

    delete test aws-cdk-v3

--- backend_cdk/sampleClient.js
+++
@@ -1,8 +0,0 @@
-const { S3Client } = require("@aws-sdk/client-s3");
-const { fromIni } = require("@aws-sdk/credential-providers");
-
-// Set the AWS Region.
-const REGION = "us-east-1"; //e.g. "us-east-1"
-// Create an Amazon S3 service client object.
-const s3Client = new S3Client({ region: REGION, credentials: fromIni({profile: 'personal'})});
-module.exports = { s3Client };
