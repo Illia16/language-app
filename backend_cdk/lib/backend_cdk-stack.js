40265eebc05fc650fac283a5814a835c1aaafe66
Illia16 <illia2143@gmail.com>
2023-10-07 14:50:03 -0400

    cdk dynamoDb resource created

--- backend_cdk/lib/backend_cdk-stack.js
+++ backend_cdk/lib/backend_cdk-stack.js
@@ -2,6 +2,7 @@ const cdk = require('aws-cdk-lib');
 const s3 = require('aws-cdk-lib/aws-s3');
 const cloudfront = require('aws-cdk-lib/aws-cloudfront');
 const lambda = require('aws-cdk-lib/aws-lambda');
+const dynamoDb = require('aws-cdk-lib/aws-dynamodb');
 const path = require('path')
 
 class BackendCdkStack extends cdk.Stack {
@@ -46,6 +47,31 @@ class BackendCdkStack extends cdk.Stack {
         },
       ],
     });
+
+    const myTable = new dynamoDb.TableV2(this, `dynamoDb-personal-project--language-app-${props.env.stage}`, {
+      tableName: `dynamoDb-personal-project--language-app-${props.env.stage}`,
+      partitionKey: {
+        name: 'user',
+        type: dynamoDb.AttributeType.STRING
+      },
+      sortKey: {
+        name: 'languageStudying',
+        type: dynamoDb.AttributeType.STRING
+      }
+    });
+
+    // user
+    // languageStudying
+    // userId
+    // languageMortherTongue
+    // item (item to study)
+    // itemType (tenses, words, sentanses)
+    // itemTypeCategory (for various languages: presentPerfect, modal verbs, cooking words, mandarinChar)
+    // itemCorrect (translation to languageMortherTongue)
+    // level (user mastery lvl for the item)
+
+    // test script
+    //aws dynamodb put-item --table-name dynamoDb-personal-project--language-app-test --profile personal --region us-east-1 --item  "{\"user\": {\"S\": \"illia\"}, \"languageStudying\": {\"S\": \"cn\"}, \"userId\": {\"S\": \"live1605\"}, \"languageMortherTongue\": {\"S\": \"ru\"}, \"item\": {\"S\": \"我\"}, \"itemType\": {\"S\": \"word\"}, \"itemTypeCategory\": {\"S\": \"mandarinChar\"}, \"itemCorrect\": {\"S\": \"Я\"}, \"level\": {\"S\": \"0\"}}"
   }
 }
 
