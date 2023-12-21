56df485c57eae132b16e488c8424bcaf9ccd78e1
Illia16 <illia2143@gmail.com>
2023-12-20 23:35:14 -0500

    loading state show over everything

--- components/Loading.vue
+++ components/Loading.vue
@@ -11,12 +11,12 @@
 <style lang="scss">
 
 .loading-container {
-    @apply fixed;
+    @apply fixed z-50;
 
     .loading-shadow {
         @apply fixed bg-white opacity-80 inset-0 z-[5];
     }
-    
+
     .loading {
         @apply fixed w-12 h-12 z-10 left-[calc(100vw/2-3rem/2)] top-[calc(100vh/2-3rem/2)] border-4 border-solid border-mainGreen border-b-transparent rounded-full inline-block;
         animation: loadingAnimation 1.25s linear infinite;
