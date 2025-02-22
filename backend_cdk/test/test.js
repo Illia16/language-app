d495d1f49582ee1e24d1d033ea34a01a7d9a62ba
Illia16 <illia2143@gmail.com>
2025-02-22 13:51:26 -0500

    wip: finish unit test for users-sqs fn

--- backend_cdk/test/test.js
+++ backend_cdk/test/test.js
@@ -8,7 +8,7 @@ const { handler: usersSqsHandler } = require('../lib/functions/users-sqs');
 
 const { findAll, findAllByPrimaryKey, findUserByEmail, getSecret, saveBatchItems } = require('../lib/functions/helpers');
 const { isAiDataValid } = require('../lib/functions/helpers/openai');
-const { SSMClient, PutParameterCommand } = require('@aws-sdk/client-ssm');
+const { SSMClient, PutParameterCommand, GetParametersCommand } = require('@aws-sdk/client-ssm');
 const { SESClient, SendEmailCommand, VerifyEmailIdentityCommand } = require("@aws-sdk/client-ses");
 
 // Mock the DynamoDB Document Client
@@ -25,7 +25,10 @@ jest.mock('../lib/functions/helpers', () => ({
   findUserByEmail: jest.fn(),
   getSecret: jest.fn(),
   saveBatchItems: jest.fn(),
-  isAiDataValid: jest.fn,
+}));
+
+jest.mock('../lib/functions/helpers/openai', () => ({
+  isAiDataValid: jest.fn(),
 }));
 
 describe('manage-users lambda', () => {
@@ -158,18 +161,94 @@ describe('Users SQS: forgot-password, delete-account, change-password, verify-em
     jest.clearAllMocks();
   });
 
-  const eventNames = ['forgot-password', 'forgot-password', 'change-password', 'verify-email', 'parse-ai-data'];
+  const eventNames = ['forgot-password', 'delete-account', 'change-password', 'verify-email', 'parse-ai-data'];
+
+  it('should forgot password: success', async () => {
+    const mockEvent = {
+      Records: [{
+        body: JSON.stringify({
+          eventName: 'forgot-password',
+          dbUsers: 'test-users-table',
+          userEmail: 'test@example.com'
+        })
+      }]
+    };
+
+    getSecret.mockResolvedValue('test-secret-key');
+
+    findUserByEmail.mockResolvedValue({
+      user: 'test-user',
+      role: 'regular',
+      userId: 'test-user-id',
+      userEmail: 'test@example.com',
+      userMotherTongue: 'en'
+    });
+
+    sesMock.on(SendEmailCommand).resolves({});
+
+    // Execute the handler
+    await usersSqsHandler(mockEvent);
+    expect(getSecret).toHaveBeenCalledWith('test-project--ssm-auth--test');
+    expect(findUserByEmail).toHaveBeenCalledWith('test-users-table', 'test@example.com');
+    expect(sesMock.calls()).toHaveLength(1);
+  });
+  it('should forgot password: user not found', async () => {
+    const mockEvent = {
+      Records: [{
+        body: JSON.stringify({
+          eventName: 'forgot-password',
+          dbUsers: 'test-users-table',
+          userEmail: 'test@example.com'
+        })
+      }]
+    };
+    getSecret.mockResolvedValue('test-secret-key');
+    findUserByEmail.mockResolvedValue(undefined);
+
+    // Execute the handler
+    await usersSqsHandler(mockEvent);
+    expect(getSecret).toHaveBeenCalled();
+    expect(findUserByEmail).toHaveBeenCalled();
+    expect(sesMock.calls()).toHaveLength(0);
+    expect(sesMock).not.toHaveReceivedCommandWith(SendEmailCommand);
+  })
+
+  it('should delete account', async () => {
+    const mockEvent = {
+      Records: [{
+        body: JSON.stringify({
+          eventName: 'delete-account',
+          dbUsers: 'test-db',
+          username: 'test-user',
+          userId: 'test-user-id',
+          toBeDeleted: 'delete'
+        })
+      }]
+    };
+
+    ddbMock.on(UpdateCommand).resolves({})
+    // Execute the handler
+    await usersSqsHandler(mockEvent);
+    expect(ddbMock.calls()).toHaveLength(1);
+    expect(ddbMock).toHaveReceivedCommandWith(UpdateCommand, {
+      TableName: 'test-db',
+      Key: {
+        user: 'test-user',
+        userId: 'test-user-id',
+      },
+    });
+  })
 
   it('should change password', async () => {
     const mockEvent = {
       Records: [{
-          body: JSON.stringify({
-              eventName: 'change-password',
-              dbUsers: 'test-db',
-              user: 'test-user',
-              userId: 'test-user-id',
-              password: 'test-user-password'
-          })
+        body: JSON.stringify({
+          eventName: 'change-password',
+          dbUsers: 'test-db',
+          user: 'test-user',
+          userId: 'test-user-id',
+          password: 'test-user-password'
+        })
       }]
     };
 
@@ -180,8 +259,8 @@ describe('Users SQS: forgot-password, delete-account, change-password, verify-em
     expect(ddbMock).toHaveReceivedCommandWith(UpdateCommand, {
       TableName: 'test-db',
       Key: {
-          user: 'test-user',
-          userId: 'test-user-id',
+        user: 'test-user',
+        userId: 'test-user-id',
       },
     });
   });
@@ -189,10 +268,10 @@ describe('Users SQS: forgot-password, delete-account, change-password, verify-em
   it('should verify-email', async () => {
     const mockEvent = {
       Records: [{
-          body: JSON.stringify({
-              eventName: 'verify-email',
-              userEmail: 'test@example.com'
-          })
+        body: JSON.stringify({
+          eventName: 'verify-email',
+          userEmail: 'test@example.com'
+        })
       }]
     };
 
@@ -208,13 +287,99 @@ describe('Users SQS: forgot-password, delete-account, change-password, verify-em
     expect(sesCalls).toHaveLength(1);
   });
 
+  it('should parse-ai-data: success', async () => {
+    const mockData = {
+      eventName: 'parse-ai-data',
+      data: {
+        aiData: {
+          items: [
+            {
+              item: 'test-item',
+              itemCorrect: 'test-item-correct',
+              itemTranscription: 'test-item-transcription',
+              itemType: 'test-item-type',
+              itemTypeCategory: 'test-item-type-category',
+              incorrectItems: ['test-incorrect-item-1', 'test-incorrect-item-2', 'test-incorrect-item-3']
+            }
+          ]
+        },
+        userData: {
+          user: 'test-user',
+          userMotherTongue: 'en',
+          languageStudying: 'es',
+          userTierPremium: true,
+          numberOfItems: 1,
+        }
+      }
+    };
+
+    const mockEvent = {
+      Records: [{
+        body: JSON.stringify(mockData)
+      }]
+    };
+
+    isAiDataValid.mockReturnValue({ isValid: true });
+    saveBatchItems.mockResolvedValue({});
+
+    await usersSqsHandler(mockEvent);
+    expect(saveBatchItems).toHaveBeenCalledWith(
+      mockData.data.aiData.items,
+      mockData.data.userData.userTierPremium,
+      mockData.data.userData.user,
+      mockData.data.userData.userMotherTongue,
+      mockData.data.userData.languageStudying
+    );
+  });
+
+  it('should parse-ai-data: isAiDataValid invalid', async () => {
+    const mockData = {
+      eventName: 'parse-ai-data',
+      data: {
+        aiData: {
+          items: [
+            {
+              item: 'test-item',
+              itemCorrect: 'test-item-correct',
+              itemTranscription: 'test-item-transcription',
+              itemType: 'test-item-type',
+              itemTypeCategory: 'test-item-type-category',
+              incorrectItems: ['test-incorrect-item-1', 'test-incorrect-item-2', 'test-incorrect-item-3', 'make-object-structure-invalid']
+            }
+          ]
+        },
+        userData: {
+          user: 'test-user',
+          userMotherTongue: 'en',
+          languageStudying: 'es',
+          userTierPremium: true,
+          numberOfItems: 1,
+        }
+      }
+    };
+
+    const mockEvent = {
+      Records: [{
+        body: JSON.stringify(mockData)
+      }]
+    };
+
+    isAiDataValid.mockReturnValue({ isValid: false });
+
+
+    await expect(usersSqsHandler(mockEvent)).rejects.toThrow(
+      'Failed to validate AI data. Please, redrive manually.'
+    );
+    expect(saveBatchItems).not.toHaveBeenCalled();
+  });
+
 
   it('should return nothing case no eventName match', async () => {
     const mockEvent = {
       Records: [{
-          body: JSON.stringify({
-              eventName: 'invalid-event-name',
-          })
+        body: JSON.stringify({
+          eventName: 'invalid-event-name',
+        })
       }]
     };
 
